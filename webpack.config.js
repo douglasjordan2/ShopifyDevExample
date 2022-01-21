9d22a30d8dc7a156cdbdbc54e24ee2d3aa3034d0
Douglas Jordan <douglasjordan2@gmail.com>
2022-01-21 15:05:15 -0600

    just fixed the layout of webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -9,104 +9,104 @@ const stats = mode === 'development' ? 'errors-warnings' : { children: false };
 const sass = require('node-sass');
 
 module.exports = {
-  mode: mode,
-  devtool: devtool,
-  entry: glob.sync('./src/js/bundles/**/*.js').reduce((acc, path) => {
-    const entry = path.replace(/^.*[\\\/]/, '').replace('.js','');
-    acc[entry] = path;
-    return acc;
-  }, {}),
-  output: {
-    filename: './assets/bundle.[name].js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-    alias: {
-      Styles: path.resolve(__dirname, 'src/styles/'),
-      Helpers: path.resolve(__dirname, 'src/styles/helpers/')
-    }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: './assets/bundle.[name].css.liquid',
-    }),
-    new CopyPlugin([
-      {
-        from: 'src/**/*',
-        to: '[folder]/[name].[ext]',
-        ignore: [
-          'src/js/**/*',
-          'src/styles/**/*',
-          'src/assets/**/*',
-          'src/liquid/templates/customers/*',
-          'src/liquid/snippets/**/*'
-        ],
-      },
-      {
-        from: 'src/liquid/templates/customers/*.liquid',
-        to: 'templates/[folder]/[name].[ext]',
-      },
-      {
-        from: 'src/liquid/snippets/**/*.liquid',
-        to: 'snippets/[name].[ext]',
-        flatten: true
-      },
-      {
-        from: 'src/assets/**/*',
-        to: 'assets/',
-        flatten: true
-      },
-      {
-				from: 'src/styles/sections/**/*',
-				to: 'assets/section.[name].css',
-				flatten: true,
-				transform(content, path) {
-					const result = sass.renderSync({
-						file: path,
-					});
-					return result.css.toString();
-				},
-			}
-    ])
-  ],
-  stats: stats,
-  module: {
-    rules: [
-      {
-        test: /\.(sc|sa|c)ss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              url: false
+    mode: mode,
+    devtool: devtool,
+    entry: glob.sync('./src/js/bundles/**/*.js').reduce((acc, path) => {
+        const entry = path.replace(/^.*[\\\/]/, '').replace('.js','');
+        acc[entry] = path;
+        return acc;
+    }, {}),
+    output: {
+        filename: './assets/bundle.[name].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        alias: {
+            Styles: path.resolve(__dirname, 'src/styles/'),
+            Helpers: path.resolve(__dirname, 'src/styles/helpers/')
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+        filename: './assets/bundle.[name].css.liquid',
+        }),
+        new CopyPlugin([
+            {
+                from: 'src/**/*',
+                to: '[folder]/[name].[ext]',
+                ignore: [
+                    'src/js/**/*',
+                    'src/styles/**/*',
+                    'src/assets/**/*',
+                    'src/liquid/templates/customers/*',
+                    'src/liquid/snippets/**/*'
+                ],
+            },
+            {
+                from: 'src/liquid/templates/customers/*.liquid',
+                to: 'templates/[folder]/[name].[ext]',
+            },
+            {
+                from: 'src/liquid/snippets/**/*.liquid',
+                to: 'snippets/[name].[ext]',
+                flatten: true
+            },
+            {
+                from: 'src/assets/**/*',
+                to: 'assets/',
+                flatten: true
+            },
+            {
+                from: 'src/styles/sections/**/*',
+                to: 'assets/section.[name].css',
+                flatten: true,
+                transform(content, path) {
+                    const result = sass.renderSync({
+                        file: path,
+                    });
+                    return result.css.toString();
+                },
             }
-          },
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+        ])
+    ],
+    stats: stats,
+    module: {
+        rules: [
+            {
+                test: /\.(sc|sa|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false
+                        }
+                    },
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    }
+                ]
             }
-          }
         ]
-      }
-    ]
-  }
+    }
 }
 
 if (mode === 'development') {
-  module.exports.plugins.push(
-    new WebpackShellPluginNext({
-      onBuildStart:{
-        scripts: ['echo Development - Webpack build in progress...🛠'],
-      }, 
-      onBuildEnd:{
-        scripts: ['echo Build Complete 📦','theme watch','theme open'],
-        parallel: true
-      }
-    })
-  )
+    module.exports.plugins.push(
+        new WebpackShellPluginNext({
+            onBuildStart:{
+            scripts: ['echo Development - Webpack build in progress...🛠'],
+            }, 
+            onBuildEnd:{
+            scripts: ['echo Build Complete 📦','theme watch','theme open'],
+            parallel: true
+            }
+        })
+    )
 }
 
 if (mode === 'production') {
